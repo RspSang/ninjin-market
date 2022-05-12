2cb0761a5dc61b0c5a7d07535260d9bc7eec7eff
HyeonJun Park <akwek33@naver.com>
2022-05-12 12:06:10 +0900

    Add layout UI

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="divide-y-[1px] py-10 ">
-      {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex cursor-pointer items-center space-x-3 px-4 py-3"
-        >
-          <div className="h-12 w-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">パク</p>
-            <p className="text-sm  text-gray-500">
-              明日、昼2時すぎに後免駅の前で会おう！
-            </p>
+    <Layout hasTabBar title="チャット">
+      <div className="divide-y-[1px] py-10 ">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="flex cursor-pointer items-center space-x-3 px-4 py-3"
+          >
+            <div className="h-12 w-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">パク</p>
+              <p className="text-sm  text-gray-500">
+                明日、昼2時すぎに後免駅の前で会おう！
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
