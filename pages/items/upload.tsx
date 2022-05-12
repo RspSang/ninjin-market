a7a0b7e5f57fb6dfdbacd494ca72beb8fe7238ab
HyeonJun Park <akwek33@naver.com>
2022-05-12 11:03:31 +0900

    Fix item upload page UI

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="space-y-5 px-4 py-10">
       <div>
         <label className="flex h-48 w-full cursor-pointer items-center justify-center rounded-md border-2 border-dashed border-gray-300 text-gray-600 hover:border-orange-500 hover:text-orange-500">
           <svg
@@ -22,16 +22,32 @@ const Upload: NextPage = () => {
           <input className="hidden" type="file" />
         </label>
       </div>
-      <div className="my-5">
+      <div>
+        <label
+          className="mb-1 block text-sm font-medium text-gray-700"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div className="relative flex items-center  rounded-md shadow-sm">
+          <input
+            id="name"
+            type="email"
+            className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+            required
+          />
+        </div>
+      </div>
+      <div>
         <label
           className="mb-1 block text-sm font-medium text-gray-700"
           htmlFor="price"
         >
-          販売価格
+          Price
         </label>
-        <div className="relative flex items-center rounded-md shadow-sm">
+        <div className="relative flex items-center  rounded-md shadow-sm">
           <div className="pointer-events-none absolute left-0 flex items-center justify-center pl-3">
-            <span className="text-sm text-gray-500">￥</span>
+            <span className="text-sm text-gray-500">$</span>
           </div>
           <input
             id="price"
@@ -40,22 +56,26 @@ const Upload: NextPage = () => {
             placeholder="0.00"
           />
           <div className="pointer-events-none absolute right-0 flex items-center pr-3">
-            <span className="text-gray-500">JPY</span>
+            <span className="text-gray-500">USD</span>
           </div>
         </div>
       </div>
       <div>
-        <label className="mb-1 block text-sm font-medium text-gray-700">
-          商品の説明
+        <label
+          htmlFor="description"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          Description
         </label>
 
         <textarea
+          id="description"
           className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 "
           rows={4}
         />
       </div>
-      <button className="mt-5 w-full rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 ">
-        出品する
+      <button className=" w-full rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 ">
+        Upload item
       </button>
     </div>
   );
