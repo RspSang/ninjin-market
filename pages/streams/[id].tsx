132638bb1eaa87d3ed2abb2109085f3f7f04ac77
HyeonJun Park <akwek33@naver.com>
2022-05-12 11:03:10 +0900

    Add streams page UI

---
+++ pages/streams/[id].tsx
@@ -0,0 +1,102 @@
+import type { NextPage } from "next";
+
+const Stream: NextPage = () => {
+  return (
+    <div className="space-y-4 py-10  px-4">
+      <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
+      <div className="mt-5">
+        <h1 className="text-3xl font-bold text-gray-900">iPhone 14</h1>
+        <span className="mt-3 block text-2xl text-gray-900">￥100,000</span>
+        <p className=" my-6 text-gray-700">
+          ○iPhone14 Pro ○SIMフリー ○256GB ○シエラブルー ○バッテリー最大容量100%
+          ○auで購入 残債なし ○ほぼ新品です。
+          購入後、動作確認や充電数回しました。
+          ご不明な点がございましたら、お知らせください。
+        </p>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold text-gray-900">ライブチャット</h2>
+        <div className="h-[50vh] space-y-4 overflow-y-scroll py-10  px-4 pb-16">
+          <div className="flex items-start space-x-2">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>
+                ご購入ありがとうございます。
+                短い間ですが、よろしくお願いします。
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>こちらこそ、よろしくお願いします。</p>
+            </div>
+          </div>
+          <div className="flex  items-start space-x-2 ">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>商品の受け渡しは明日14時、市役所前でいかがでしょうか</p>
+            </div>
+          </div>
+          <div className="flex items-start space-x-2">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>
+                ご購入ありがとうございます。
+                短い間ですが、よろしくお願いします。
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>こちらこそ、よろしくお願いします。</p>
+            </div>
+          </div>
+          <div className="flex  items-start space-x-2 ">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>商品の受け渡しは明日14時、市役所前でいかがでしょうか</p>
+            </div>
+          </div>
+          <div className="flex items-start space-x-2">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>
+                ご購入ありがとうございます。
+                短い間ですが、よろしくお願いします。
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>こちらこそ、よろしくお願いします。</p>
+            </div>
+          </div>
+          <div className="flex  items-start space-x-2 ">
+            <div className="h-8 w-8 rounded-full bg-slate-400" />
+            <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
+              <p>商品の受け渡しは明日14時、市役所前でいかがでしょうか</p>
+            </div>
+          </div>
+        </div>
+        <div className="fixed inset-x-0 bottom-0  bg-white py-2">
+          <div className="relative mx-auto flex w-full  max-w-md items-center">
+            <input
+              type="text"
+              className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+            />
+            <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+              <button className="flex items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+                &rarr;
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stream;
