20412761b50dcff2f7d3d81f0eb80b1fa7a97b2c
HyeonJun Park <akwek33@naver.com>
2022-05-18 14:43:59 +0900

    Add similar products function

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ interface ProductsResponse {
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
   const { data } = useSWR<ProductsResponse>("/api/products");
-  console.log(data);
   return (
     <Layout title="ホーム" hasTabBar>
       <Head>
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,8 +1,26 @@
 import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Link from "next/link";
+import { Product, User } from "@prisma/client";
+
+interface ProductWithUser extends Product {
+  user: User;
+}
+
+interface ItemDetailResponse {
+  ok: boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[];
+}
 
 const ItemDetail: NextPage = () => {
+  const router = useRouter();
+  const { data, error } = useSWR<ItemDetailResponse>(
+    router.query.id ? `/api/products/${router.query.id}` : null
+  );
   return (
     <Layout canGoBack>
       <div className="px-4 py-4">
@@ -11,21 +29,24 @@ const ItemDetail: NextPage = () => {
           <div className="flex cursor-pointer items-center space-x-3 border-t border-b py-3">
             <div className="h-12 w-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">高知太郎</p>
-              <p className="text-xs font-medium text-gray-500">
-                プロフィールを見る &rarr;
+              <p className="text-sm font-medium text-gray-700">
+                {data?.product?.user?.name}
               </p>
+              <Link href={`/users/profiles/${data?.product?.user?.id}`}>
+                <a className="text-xs font-medium text-gray-500">
+                  プロフィールを見る &rarr;
+                </a>
+              </Link>
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">iPhone 14</h1>
-            <span className="mt-3 block text-3xl text-gray-900">￥100,000</span>
-            <p className=" my-6 text-gray-700">
-              ○iPhone14 Pro ○SIMフリー ○256GB ○シエラブルー
-              ○バッテリー最大容量100% ○auで購入 残債なし ○ほぼ新品です。
-              購入後、動作確認や充電数回しました。
-              ご不明な点がございましたら、お知らせください。
-            </p>
+            <h1 className="text-3xl font-bold text-gray-900">
+              {data?.product?.name}
+            </h1>
+            <span className="mt-3 block text-3xl text-gray-900">
+              ￥{data?.product?.price}
+            </span>
+            <p className=" my-6 text-gray-700">{data?.product?.description}</p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="購入手続きへ" />
               <button className="flex items-center justify-center rounded-md p-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
@@ -53,14 +74,18 @@ const ItemDetail: NextPage = () => {
             この商品を見ている人におすすめ
           </h2>
           <div className=" mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
-                <div className="mb-4 h-56 w-full rounded-md bg-slate-300" />
-                <h3 className="-mb-1 text-gray-700">Galaxy S60</h3>
-                <span className="text-sm font-medium text-gray-900">
-                  ￥90,000
-                </span>
-              </div>
+            {data?.relatedProducts?.map((product) => (
+              <Link href={`/products/${product.id}`} key={product.id}>
+                <a>
+                  <div>
+                    <div className="mb-4 h-56 w-full rounded-md bg-slate-300" />
+                    <h3 className="-mb-1 text-gray-700">{product.name}</h3>
+                    <span className="text-sm font-medium text-gray-900">
+                      ￥{product.price}
+                    </span>
+                  </div>
+                </a>
+              </Link>
             ))}
           </div>
         </div>
