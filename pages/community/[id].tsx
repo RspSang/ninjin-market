83620791751e8130ee6a5d05656ac5b5faf650fc
HyeonJun Park <akwek33@naver.com>
2022-05-19 10:51:49 +0900

    Add post detail

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,8 +1,34 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import { Answer, Post, User } from "@prisma/client";
+import Link from "next/link";
+
+interface AnswerWithUser extends Answer {
+  user: User;
+}
+
+interface PostWithUser extends Post {
+  user: User;
+  answers: AnswerWithUser[];
+  _count: {
+    answers: number;
+    wondering: number;
+  };
+}
+
+interface CommunityPostResponse {
+  ok: boolean;
+  post: PostWithUser;
+}
 
 const CommunityPostDetail: NextPage = () => {
+  const router = useRouter();
+  const { data, error } = useSWR<CommunityPostResponse>(
+    router.query.id ? `/api/posts/${router.query.id}` : null
+  );
   return (
     <Layout canGoBack>
       <div>
@@ -12,16 +38,20 @@ const CommunityPostDetail: NextPage = () => {
         <div className="mb-3 flex cursor-pointer items-center space-x-3  border-b px-4 pb-3">
           <div className="h-10 w-10 rounded-full bg-slate-300" />
           <div>
-            <p className="text-sm font-medium text-gray-700">パク</p>
-            <p className="text-xs font-medium text-gray-500">
-              プロフィールを見る &rarr;
+            <p className="text-sm font-medium text-gray-700">
+              {data?.post?.user?.name}
             </p>
+            <Link href={`/users/profiles/${data?.post?.user?.id}`}>
+              <a className="text-xs font-medium text-gray-500">
+                プロフィールを見る &rarr;
+              </a>
+            </Link>
           </div>
         </div>
         <div>
           <div className="mt-2 px-4 text-gray-700">
             <span className="font-medium text-orange-500">Q.</span>
-            美味しいラーメン屋さんを教えてください。
+            {data?.post?.question}
           </div>
           <div className="mt-3 flex w-full space-x-5 border-t border-b-[2px] px-4 py-2.5  text-gray-700">
             <span className="flex items-center space-x-2 text-sm">
@@ -39,7 +69,7 @@ const CommunityPostDetail: NextPage = () => {
                   d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
-              <span>気になる 1</span>
+              <span>気になる {data?.post?._count?.wondering}</span>
             </span>
             <span className="flex items-center space-x-2 text-sm">
               <svg
@@ -56,23 +86,25 @@ const CommunityPostDetail: NextPage = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              <span>回答 1</span>
+              <span>回答 {data?.post?._count?.answers}</span>
             </span>
           </div>
         </div>
         <div className="my-5 space-y-5 px-4">
-          <div className="flex items-start space-x-3">
-            <div className="h-8 w-8 rounded-full bg-slate-200" />
-            <div>
-              <span className="block text-sm font-medium text-gray-700">
-                lez
-              </span>
-              <span className="block text-xs text-gray-500 ">2時間前</span>
-              <p className="mt-2 text-gray-700">
-                豚太郎の味噌カツラーメンが美味しいよ！
-              </p>
+          {data?.post.answers.map((answer) => (
+            <div key={answer.id} className="flex items-start space-x-3">
+              <div className="h-8 w-8 rounded-full bg-slate-200" />
+              <div>
+                <span className="block text-sm font-medium text-gray-700">
+                  {answer.user.name}
+                </span>
+                <span className="block text-xs text-gray-500 ">
+                  {answer.createdAt.toString()}
+                </span>
+                <p className="mt-2 text-gray-700">{answer.answer}</p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
         <div className="px-4">
           <TextArea
