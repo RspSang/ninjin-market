99b87008f0013a88b65865732878a286775e7d7c
HyeonJun Park <akwek33@naver.com>
2022-05-24 10:06:53 +0900

    Add edit profile image

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -4,11 +4,12 @@ import Input from "@components/input";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
 import { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useMutation from "@libs/client/useMutation";
 import { useRouter } from "next/router";
 
 interface EditProfileForm {
+  avatar?: FileList;
   name?: string;
   email?: string;
   phone?: string;
@@ -29,22 +30,42 @@ const EditProfile: NextPage = () => {
     setValue,
     setError,
     formState: { errors },
+    watch,
   } = useForm<EditProfileForm>();
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const onValid = async ({ avatar, email, phone, name }: EditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name === "") {
       return setError("formErrors", {
         message: "メールまたは携帯番号のいずれかを入力してください",
       });
     }
-    editProfile({ email, phone, name });
+    if (avatar && avatar.length > 0 && user) {
+      const { uploadURL } = await (await fetch(`/api/files`)).json();
+      const form = new FormData();
+      form.append("file", avatar[0], user?.id + "");
+      const {
+        result: { id },
+      } = await (
+        await fetch(uploadURL, {
+          method: "POST",
+          body: form,
+        })
+      ).json();
+      editProfile({ email, phone, name, avatarId: id });
+    } else {
+      editProfile({ email, phone, name });
+    }
   };
   useEffect(() => {
     if (user?.name) setValue("name", user.name);
     if (user?.email) setValue("email", user.email);
     if (user?.phone) setValue("phone", user.phone);
+    if (user?.avatar)
+      setAvatarPreview(
+        `https://imagedelivery.net/GSDuBVO5Xp3QfdrHmnLc2A/${user?.avatar}/avatar`
+      );
     if (data?.ok === true) {
       router.push(`/profile`);
     }
@@ -52,17 +73,33 @@ const EditProfile: NextPage = () => {
       setError("formErrors", { message: data.error });
     }
   }, [user, data, setError, setValue, router]);
+  const [avatarPreview, setAvatarPreview] = useState("");
+  const avatar = watch("avatar");
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
   return (
     <Layout canGoBack title="プロフィールの更新">
       <form onSubmit={handleSubmit(onValid)} className="space-y-4 py-10 px-4">
         <div className="flex items-center space-x-3">
-          <div className="h-14 w-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="h-14 w-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="h-14 w-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer rounded-md border border-gray-300 py-2 px-3 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"
           >
             写真を選ぶ
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
